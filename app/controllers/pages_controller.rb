2c2e41ad04bf0954f9cc5237a19fdc6dd21212a7
Julien Abello <ghostofavalon@gmail.com>
2022-08-31 11:24:27 +0200

    wip controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,11 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    # @user = current_user
-    # if @user.status == true
-    #   redirect_to gm_path
-    # else
-    #   redirect_to player_path
-    # end
+    @user = current_user
+    @user.status = params[:status]
+    @user.save
   end
 end
