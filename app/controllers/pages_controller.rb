a45f7fd8892b7a5faa0893b4c8371aae2e5105cd
Julien Abello <ghostofavalon@gmail.com>
2022-08-31 14:41:40 +0200

    todo debug

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,40 @@
 class PagesController < ApplicationController
-
+  before_action :set_character, only: [:update_job]
   def home
     @user = current_user
     @user.status = params[:status]
     @user.save
+  end
+
+  def dysplay_job
+    @jobs = Job.all
+    @user = current_user
+  end
+
+  def dysplay_race
+    @races = Race.all
+    @user = current_user
+  end
+
+  def update_race
+    @character.race = params[:race]
+    @character.save
+    redirect_to job_path
+  end
+
+  def update_job
+    @character.job = params[:job]
+    @character.save
+    redirect_to characterisics_list_path
+  end
+
+  private
+
+  def character_params
+    params.require(:character).permit(:job)
+  end
+
+  def set_character
+    @character = Character.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+
   resources :rooms do
     resources :characters, except: %i[edit]
+    get 'characters/:id/race', to: 'pages#display_race', as: "set_race"
   end
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
