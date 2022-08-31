ba2ba28347ce396f0b95184f8f872a9ce6dceb9e
Anan Mignot <anan.wav@gmail.com>
2022-08-31 16:27:33 +0200

    Improved Rooms index and show, and pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
+  get 'character/:id/race', to: 'pages#races_index', as: 'set_race'
+
   resources :rooms do
     resources :characters, except: %i[edit]
   end
-  get 'character/:id/race', to: 'pages#display_race', as: 'set_race'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
