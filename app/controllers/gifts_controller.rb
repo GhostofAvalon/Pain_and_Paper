7d1ee029ee24ef0cfc52cf5d9654215e1e3490c4
Julien Abello <ghostofavalon@gmail.com>
2022-09-02 07:41:16 +0200

    cleaned gift controller

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -5,21 +5,4 @@ class GiftsController < ApplicationController
     @character_gift = CharacterGift.new
     @n = rand(1..5)
   end
-
-  def create
-    @gift = Gift.find(params[:id])
-    @character_gift.character_id = params[:character_id]
-    redirect_to spells_path
-  end
-
-  def add
-    @character.gifts << @gift
-    puts "Added #{@gift.name} to #{@character.name}"
-  end
-
-  private
-
-  def gift_params
-    params.require(:gift).permit(:name, :description)
-  end
 end
